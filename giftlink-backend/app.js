a796d3b80fdb079f9b6e5d24864ba4e9f5d3ff96
docoffuture <166705217+docoffuture@users.noreply.github.com>
2024-08-25 00:50:19 +0200

    Update app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,50 +1,29 @@
-/*jshint esversion: 8 */
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const pinoLogger = require('./logger');
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import MainPage from './components/MainPage/MainPage';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
+import SearchPage from './components/SearchPage/SearchPage';
+import Profile from './components/Profile/Profile';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+function App() {
 
-const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
-
-
-const app = express();
-app.use("*",cors());
-const port = 3060;
-
-// Connect to MongoDB; we just do this one time
-connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
-})
-    .catch((e) => console.error('Failed to connect to DB', e));
-
-
-app.use(express.json());
-
-// Route files
-const giftRoutes = require('./routes/giftRoutes');
-const authRoutes = require('./routes/authRoutes');
-const searchRoutes = require('./routes/searchRoutes');
-const pinoHttp = require('pino-http');
-const logger = require('./logger');
-
-app.use(pinoHttp({ logger }));
-
-// Use Routes
-app.use('/api/gifts', giftRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/search', searchRoutes);
-
-// Global Error Handler
-app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-});
-
-app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+  return (
+        <>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/app" element={<MainPage />} />
+          <Route path="/app/login" element={<LoginPage/>} />
+          <Route path="/app/register" element={<RegisterPage />} />
+          <Route path="/app/product/:productId" element={<DetailsPage/>} />
+          <Route path="/app/search" element={<SearchPage/>} />
+          <Route path="/app/profile" element={<Profile/>} />
+        </Routes>
+        </>
+  );
+}
+export default App;
